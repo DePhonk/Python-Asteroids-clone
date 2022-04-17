bd04ed7e7c816355972b9fbea2a260134a137ac6
DePhonk <76873834+DePhonk@users.noreply.github.com>
2022-04-18 00:09:54 +0300

    Update game.py

--- Game_Project/game.py
+++ Game_Project/game.py
@@ -2,8 +2,6 @@
 from GameObject import *
 from settings import loader, pos_rand
 import pygame
-
-
 class Main: #Основной класс
     MIN_DIST = 200
     # __init__ - иннициализация PyGame, установка размера и названия экрана
@@ -36,38 +34,31 @@ class Main: #Основной класс
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 quit()
-
             elif (self.player
                   and event.type == pygame.KEYDOWN
                   and event.key == pygame.K_ESCAPE):
                 self.player.shoot()
-
         key = pygame.key.get_pressed()
-
         if self.player:
             if key[pygame.K_d]:
                 self.player.rotate(clockwise=True)
             elif key[pygame.K_a]:
                 self.player.rotate(clockwise=False)
-
             if key[pygame.K_w]:
                 self.player.fow_accelerate()
             elif key[pygame.K_s]:
                 self.player.back_accelerate()
-
             if key[pygame.K_SPACE]:
                 self.player.shoot()
     #Обработка игровых процессов(логики)
     def _game_process(self):
         for obj in self._get_obj():
             obj.obj_move(self.screen)
-
         if self.player:
             for meteor in self.meteor:
                 if meteor.obj_collide(self.player):
                     self.player = None
                     break
-
         for bullet in self.bullets[:]:
             for meteor in self.meteor[:]:
                 if meteor.obj_collide(bullet):
@@ -75,23 +66,18 @@ class Main: #Основной класс
                     self.bullets.remove(bullet)
                     meteor.split()
                     break
-
         for bullet in self.bullets[:]:
             if not self.screen.get_rect().collidepoint(bullet.pos):
                 self.bullets.remove(bullet)
     #Обработка и прорисовка объектов
     def _drawing(self):
         self.screen.blit(self.background, (0, 0))
-
         for obj in self._get_obj():
             obj.obj_draw(self.screen)
         pygame.display.flip()
         self.clock.tick(50)
-
     def _get_obj(self):
         game_obj = [*self.meteor, *self.bullets]
-
         if self.player:
             game_obj.append(self.player)
-
         return game_obj
