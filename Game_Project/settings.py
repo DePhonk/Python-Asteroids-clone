70a82e1f82d4374fe770c75a903e6f4e9191bb54
DePhonk <76873834+DePhonk@users.noreply.github.com>
2022-04-18 00:31:50 +0300

    Update settings.py

--- Game_Project/settings.py
+++ Game_Project/settings.py
@@ -1,8 +1,8 @@
+#settings.py
 from pygame.image import load
 from pygame.math import Vector2
 import random
-
-
+#Загрузчик спрайтов из ассетов
 def loader(name, alpha=True):
     path = f'assets/{name}.png'
     loaded_sprite = load(path)
@@ -10,20 +10,18 @@ def loader(name, alpha=True):
         return loaded_sprite.convert()
     else:
         return loaded_sprite.convert_alpha()
-
-
+#Переход на противоположную сторону, если объект выходит из поля видимости игрока (Метеориты и сам игрок)
 def wrap_pos(pos, surface):
     x, y = pos
     w, h = surface.get_size()
     return Vector2(x % w, y % h)
-
-
+#Случайное положение Метеорита, основываясь на векторах ширины и высоты игрового поля
 def pos_rand(surface):
     return Vector2(
         random.randrange(surface.get_width()),
         random.randrange(surface.get_height()),
     )
-
+#Установка случайной скорости
 def vel_rand(min_speed, max_speed):
     speed = random.randint(min_speed, max_speed)
     angle = random.randrange(0, 360)
